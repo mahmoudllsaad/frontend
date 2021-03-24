b3b8b673a3f9c1b2eb8fdc272a11257348627201
mahmoudllsaad <mahmoudllahmedllsaad@gmail.com>
2021-03-24 16:03:31 +0200

    Add existing file second

--- src/components/CreateBook.js
+++ src/components/CreateBook.js
@@ -34,7 +34,7 @@ class CreateBook extends Component {
     };
 
     axios
-      .post('http://localhost:8082/api/books', data)
+      .post('https://fullmernstack.herokuapp.com/api/books', data)
       .then(res => {
         this.setState({
           title: '',
